0c9c19649002d7424123ce3a25a92782b5cb1629
hiki-long <411046642@qq.com>
2020-05-12 21:22:27 +0800

    做好音频处理，但视频处理未测试的版本，此外还未修复显示界面未显示音乐封面的BUG

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,14 @@
 package application;
 	
+import java.io.*;
 import java.net.URL;
 import java.net.URLDecoder;
 
+import controller.MainController;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -14,34 +17,21 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-//			Slider sl = new Slider(arg0, arg1, arg2)
-			//请注意这里替换为自己电脑内音频或者视频的文件路径
-			URL url = this.getClass().getClassLoader().getResource("res/藍井エイル - 星が降るユメ.mp3");
-			
-			/*只是用来显示正常的文件名，否则会有汉字之类的无法正常显示"
-			String path = URLDecoder.decode(url.toExternalForm(),"utf-8");
-			*/
-			
-			//这里用来出错时测试url是否正常读入
-			System.out.println(url);
-			//读取媒体文件
-			Media media = new Media(url.toExternalForm());
-			//这里是播放音乐的控制器
-			MediaPlayer mp = new MediaPlayer(media);
 			//读取fxml样式
 			FXMLLoader fx = new FXMLLoader();
+			
 			//载入fxml样式
 			fx.setLocation(fx.getClassLoader().getResource("fxml/base.fxml"));
 			
 			//这里用来测试fxml是否正确读入
-			System.out.println(fx.getLocation());
-			
+//			System.out.println(fx.getLocation());
+
 			//将载入的样式显示在scene里即可。
 			AnchorPane root = (AnchorPane)fx.load();
-			
 			Scene scene = new Scene(root,1280,720);
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,29 +1,254 @@
 package controller;
+import java.io.*;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.*;
+
+import application.ReadDir;
+import application.ReadLrc;
+import javafx.collections.ObservableMap;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 public class MainController {
-	// TODO Auto-generated constructor stub
+
+	//类的变量
+	public static List<Media> mp3_list = new ArrayList<Media>();//记录当前mp3播放列表
+	private boolean mode = false;//读取音乐为0，读取视频为1
+	private static String media_type[]= {"mp3","wmv","flac","mp4","avi","mkv" };//读取的文件格式
+//	public String select_directory = this.getClass().getClassLoader().getResource("").toExternalForm() + "res/";//选择文件夹记录
+	public String select_directory;
+	public static ReadDir rd;//读取文件夹的类的实例化对象
+	public static ReadLrc[] store_lrc;//每个音乐文件的歌词存储
+	public MediaPlayer mp;//播放器控件
+	private int index = 0;//当前列表音乐播放的index,从0-音乐数量-1
+	public boolean isplay = false;//是否正在播放的布尔值
+	
+	@FXML
+	private Button music_directory;//这个是界面中选择文件夹的按钮
+	@FXML
+	private ImageView cover;//音乐封面
+	@FXML
+	private Button prev;
+	@FXML
+	private Button after;
+	@FXML
+	private Button play;
+	
 	public MainController()
 	{
 		
 	}
 	
-	@FXML
-	private Button prev;
-	
-	@FXML
-	public void initialize()
+	public void initialize()//界面载入初始化函数
 	{
-		System.out.println("实例化测试");
+//		System.out.println("实例化测试");
+		String xx = System.getProperty("user.dir");
+		select_directory = xx + "\\bin\\res\\";
+		System.out.println(select_directory);
+		choose_direc(select_directory);
 		
 	}
 	
-	public void click()
+	
+	
+	
+	public void read_m3ulist(boolean mode, String path)//读取文件夹生成的mp3文件的播放列表
 	{
-		System.out.println("上一首被点击");
+		if(mode == false)//mode ==0,读取音乐列表
+		{
+			try {
+				BufferedReader bf = new BufferedReader(new FileReader(path));
+				String line;
+				while((line = bf.readLine())!=null)
+				{
+//					System.out.println(line);
+					if(line.endsWith("mp3") || line.endsWith("wmv") || line.endsWith("flac")) {
+						File temp_file = new File(line);
+						String url = temp_file.toURI().toURL().toExternalForm();
+//						System.out.println(url);
+						Media media = new Media(url);
+						mp3_list.add(media);
+//						System.out.println(media);
+						MediaPlayer tplay = new MediaPlayer(media);
+					}
+				}
+//				System.out.println(mp3_list.get(0));
+				mp = new MediaPlayer(mp3_list.get(index));
+				setCover(mp);
+				bf.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		else {//mode == 1,读取视频列表 这里暂时不做处理
+			
+		}
 	}
 	
+	public void choose_direc(String path)//载入初始化
+	{
+		try {
+			System.out.println("初始化数据");
+			System.out.println(path);
+			rd = new ReadDir();
+			rd.createmusicList(select_directory, media_type);
+			for(Iterator<String> it = rd.get_MusicName().iterator(); it.hasNext();)
+			{
+				System.out.println(it.next());
+			}
+			for(Iterator<String> it = rd.get_VedioName().iterator(); it.hasNext();)
+			{
+				System.out.println(it.next());
+			}
+			store_lrc = new ReadLrc[rd.get_MusicName().size()];
+			for(int num = 0; num < rd.get_MusicName().size(); num++)
+			{	
+				String tp = select_directory + "\\" + rd.get_MusicName().get(num) + ".lrc";
+				System.out.println(tp);
+				store_lrc[num] = new ReadLrc(tp);
+			}
+			int q=0,w=0;
+			for(int num = 0; num < rd.get_MusicName().size(); num++)
+			{
+				System.out.println(store_lrc[num].getMusicName());
+				if(store_lrc[num].getMusicName().equals("无题"))
+				{
+					q++;
+				}
+				else w++;
+			}
+			System.out.println("有歌词"+ w +"首, 无歌词" + q +"首");
+			read_m3ulist(mode, select_directory + "\\music.m3u");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void choose_direc()//选择文件夹后的读取文件功能，并生成播放列表
+	{
+		DirectoryChooser dire = new DirectoryChooser();
+		dire.setTitle("请选择音乐所在文件夹");
+		dire.setInitialDirectory(new File(System.getProperty("java.class.path")));
+		File newFolder = dire.showDialog(new Stage());
+		if(newFolder!=null)
+		System.out.println(newFolder);
+		select_directory = newFolder.getPath();
+		try {
+			System.out.println("mp3文件读取");
+			rd = new ReadDir();
+			rd.createmusicList(select_directory, media_type);
+			for(Iterator<String> it = rd.get_MusicName().iterator(); it.hasNext();)
+			{
+				System.out.println(it.next());
+			}
+			for(Iterator<String> it = rd.get_VedioName().iterator(); it.hasNext();)
+			{
+				System.out.println(it.next());
+			}
+			store_lrc = new ReadLrc[rd.get_MusicName().size()];
+			for(int num = 0; num < rd.get_MusicName().size(); num++)
+			{	
+				String tp = select_directory + "\\" + rd.get_MusicName().get(num) + ".lrc";
+				System.out.println(tp);
+				store_lrc[num] = new ReadLrc(tp);
+			}
+			int q=0,w=0;
+			for(int num = 0; num < rd.get_MusicName().size(); num++)
+			{
+				System.out.println(store_lrc[num].getMusicName());
+				if(store_lrc[num].getMusicName().equals("无题"))
+				{
+					q++;
+				}
+				else w++;
+			}
+			System.out.println("有歌词"+ w +"首, 无歌词" + q +"首");
+			read_m3ulist(mode, select_directory + "\\music.m3u");
+
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void Next()//下一首
+	{
+		System.out.println(index);
+		if(index < mp3_list.size()-1)
+		{
+			index++;
+		}
+		else {
+			index = 0;
+		}
+		mp.dispose();
+		mp = new MediaPlayer(mp3_list.get(index));
+		setCover(mp);
+		mp.play();
+	}
+	
+	public void Before()//上一首
+	{
+		if(index == 0)
+		{
+			index = mp3_list.size()-1;
+		}
+		else {
+			index--;
+		}
+		mp.dispose();
+		mp = new MediaPlayer(mp3_list.get(index));
+		setCover(mp);
+		mp.play();
+	}
+	
+	public void PlayMusic()//播放按钮
+	{
+		if(!isplay) {
+			mp.play();
+			isplay = true;
+		}
+		else {
+			mp.pause();
+			isplay = false;
+		}
+	}
+	
+	public void setCover(MediaPlayer player)
+	{
+		player.setOnPlaying(new Runnable() {
+			
+			@Override
+			public void run() {
+				ObservableMap<String,Object> obmap = mp3_list.get(index).getMetadata();
+//				System.out.println("图片：" + (Image)obmap.get("image"));
+				cover.setImage((Image)obmap.get("image"));
+			}
+		});
+	}
+	
+	
+
+	public Map<Long,String> getLyric()//获取当前音乐歌词
+	{
+		return store_lrc[index].getLyric();
+	}
+	
+	public String getMusicName()//获取当前音乐名字
+	{
+		return store_lrc[index].getMusicName();
+	}
+	
+	public String getSingerName()//获取当前歌手名字
+	{
+		return store_lrc[index].getSingerName();
+	}
 	
 }
